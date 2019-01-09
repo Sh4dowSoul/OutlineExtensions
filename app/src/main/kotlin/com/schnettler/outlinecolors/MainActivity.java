2f1ea555df197708a99e31b2e1e067a910e96afe
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2019-01-09 15:11:57 +0100

    Project: Merge Telegram X Generator

---
+++ app/src/main/kotlin/com/schnettler/outlinecolors/MainActivity.java
@@ -0,0 +1,33 @@
+package com.schnettler.outlinecolors;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.preference.PreferenceManager;
+
+import com.schnettler.outlinecolors.telegram.TelegramFragment;
+
+import static android.support.v7.app.AppCompatDelegate.MODE_NIGHT_NO;
+import static android.support.v7.app.AppCompatDelegate.MODE_NIGHT_YES;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new TelegramFragment());
+        ft.commit();
+
+        getSupportActionBar().setTitle("Telegram X Generator");
+    }
+
+    @Override
+    public void setTheme(int resId) {
+        super.setTheme(resId);
+        AppCompatDelegate.setDefaultNightMode(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("dark_theme", false) ? MODE_NIGHT_YES : MODE_NIGHT_NO);
+    }
+}
